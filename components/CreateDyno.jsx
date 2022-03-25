752583c3deda34afbd6ed572dc7c0192d14f80fe
Priyangsu Banerjee <priyangsu26@gmail.com>
2022-03-26 01:45:28 +0530

    blocks overflow fixed

--- components/CreateDyno.jsx
+++ components/CreateDyno.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import * as HiIcons from "react-icons/hi";
 import * as VscIcons from "react-icons/vsc";
 import * as MdIcons from "react-icons/md";
+import * as BsIcons from "react-icons/bs";
 import { useState, useEffect } from "react";
 import CreateProfile from "./CreateProfile";
 import FetchUser from "../user/FetchUser";
@@ -15,15 +16,13 @@ function CreateDyno({ createDynoActive, setCreateDynoActive }) {
     if (user) {
       setUser(user);
     }
-  });
 
-  useEffect(() => {
     if (createDynoActive) {
       document.body.style.overflow = "hidden";
     } else {
       document.body.style.overflow = "unset";
     }
-  }, [createDynoActive]);
+  });
 
   return (
     <div>
@@ -32,10 +31,19 @@ function CreateDyno({ createDynoActive, setCreateDynoActive }) {
           style={{ backgroundColor: "rgba(0, 0, 0, 0.5)" }}
           className="fixed top-0 left-0 w-full h-full flex"
         >
-          <div className="bg-white flex flex-col rounded-t-lg md:rounded-lg p-4 mt-auto md:max-w-[500px] h-auto md:min-h-[500px] overflow-y-auto md:m-auto w-full">
+          <div className="bg-white flex flex-col rounded-t-lg md:rounded-lg p-4 mt-auto md:max-w-[500px] h-auto overflow-y-auto md:m-auto w-full">
             <div className="flex  items-center">
               <HiIcons.HiViewGridAdd className="text-2xl text-green-500 mr-2" />
-              <h1 className="text-xl font-medium">Create new</h1>
+              <h1 className="text-xl font-semibold">Create dyno</h1>
+              {user !== null ? (
+                <div
+                  onClick={() => setCreateProfileActive(true)}
+                  className="flex items-center text-gray-500 text-xs mt-[1px] ml-2 cursor-pointer"
+                >
+                  <BsIcons.BsChevronRight className="mr-1" /> {user.name}
+                </div>
+              ) : null}
+
               <MdIcons.MdClose
                 onClick={() => setCreateDynoActive(false)}
                 className="ml-auto bg-gray-50 rounded-full text-3xl p-2 cursor-pointer"
@@ -59,11 +67,33 @@ function CreateDyno({ createDynoActive, setCreateDynoActive }) {
               </div>
             ) : (
               <div className="flex flex-col h-full">
-                <p className="text-xs text-gray-500 leading-6 mt-3">
-                  Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                  Fugiat vel, deserunt minus molestias sunt incidunt.
-                </p>
-                <div className="flex flex-col md:flex-row items-center mt-4">
+                <div className="p-2 border border-green-500 mt-4 rounded flex items-center">
+                  <div className="ml-2">
+                    <p className="text-xs text-gray-500 mt-1 leading-5">
+                      <a
+                        className="font-medium text-green-500 text-sm"
+                        target={"_blank"}
+                        rel="noopener noreferrer"
+                        href="https://easyfidb.herokuapp.com/#faq-container"
+                      >
+                        EasyFi Db
+                      </a>{" "}
+                      is a free image storage solution, that can store you files
+                      &amp; images in a secure way, and can be accessed through
+                      an url.{" "}
+                      <a
+                        className="text-green-500"
+                        target={"_blank"}
+                        rel="noopener noreferrer"
+                        href="https://easyfidb.herokuapp.com/#faq-container"
+                      >
+                        Visit site{" "}
+                        <BsIcons.BsChevronRight className="inline-block mt-[1px]" />
+                      </a>
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col md:flex-row items-center mt-6">
                   <input
                     type="text"
                     placeholder="Icon image link"
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,14 +1,9 @@
 import Head from "next/head";
-import Image from "next/image";
-import CreateDyno from "../components/CreateDyno";
-import Topnav from "../components/Topnav";
 import UnderDevelopment from "../components/UnderDevelopment";
 import About from "../fragments/About";
 import SupportUkraine from "../fragments/SupportUkraine";
-import { useState } from "react";
 
 export default function Home() {
-  const [createDynoActive, setCreateDynoActive] = useState(false);
   return (
     <div>
       <Head>
